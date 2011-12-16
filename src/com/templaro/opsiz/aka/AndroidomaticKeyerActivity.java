ebf1d8d5a1329985678600956b233ecdbc609eb0
Jack Welch <dhakajack@gmail.com>
2011-12-15 23:51:04 -0500

    Canned messages now displayed in a ListView

--- src/com/templaro/opsiz/aka/AndroidomaticKeyerActivity.java
+++ src/com/templaro/opsiz/aka/AndroidomaticKeyerActivity.java
@@ -16,18 +16,22 @@
 
 package com.templaro.opsiz.aka;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
-public class AndroidomaticKeyerActivity extends Activity {
+public class AndroidomaticKeyerActivity extends ListActivity {
 	
 	private String TAG = "AndroidomaticKeyer";
 	private Thread soundThread;
@@ -42,27 +46,27 @@ public class AndroidomaticKeyerActivity extends Activity {
 	private int speed = 15;  // should be tweakable
 	private MorsePlayer player = new MorsePlayer(hertz, speed);
 	
-	/* Memory slot buttons are hard coded at this point, but down the road,
-	 * how about dropping the buttons, and just displaying the text in
-	 * a growable listview, with ability to add/delete slots in an array?
-	 */
-	
-	private Button memorySlotButton1;
-	private TextView memorySlotTextView1;
-	private Button memorySlotButton2;
-	private TextView memorySlotTextView2;
-	private Button memorySlotButton3;
-	private TextView memorySlotTextView3;
-	private Button memorySlotButton4;
-	private TextView memorySlotTextView4;
-	
-	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        String[] messages = getResources().getStringArray(R.array.messages_array);
+        setListAdapter(new ArrayAdapter<String>(this, 
+                android.R.layout.simple_list_item_1, messages));
+        
+        ListView lv = getListView();
+  	  //lv.setTextFilterEnabled(true);
+
+  	  lv.setOnItemClickListener(new OnItemClickListener() {
+  	    public void onItemClick(AdapterView<?> parent, View view,
+  	        int position, long id) {
+  	    		keyerEditText.setText(((TextView) view).getText().toString());
+  	    }
+  	  });
+        
+      
         soundThread = new Thread(new Runnable() {
 	            @Override
 	            public void run() {
@@ -72,23 +76,6 @@ public class AndroidomaticKeyerActivity extends Activity {
         
         playButton = (Button)findViewById(R.id.playButton);
         playButton.setOnClickListener(playButtonListener);
-        
-        memorySlotButton1 = (Button)findViewById(R.id.memorySlotButton1);
-        memorySlotButton1.setOnClickListener(memorySlotButtonListener1);
-        
-        memorySlotButton2 = (Button)findViewById(R.id.memorySlotButton2);
-        memorySlotButton2.setOnClickListener(memorySlotButtonListener2);
-        
-        memorySlotButton3 = (Button)findViewById(R.id.memorySlotButton3);
-        memorySlotButton3.setOnClickListener(memorySlotButtonListener3);
-        
-        memorySlotButton4 = (Button)findViewById(R.id.memorySlotButton4);
-        memorySlotButton4.setOnClickListener(memorySlotButtonListener4);
-        
-        memorySlotTextView1 = (TextView)findViewById(R.id.memorySlotTextView1);
-        memorySlotTextView2 = (TextView)findViewById(R.id.memorySlotTextView2);
-        memorySlotTextView3 = (TextView)findViewById(R.id.memorySlotTextView3);
-        memorySlotTextView4 = (TextView)findViewById(R.id.memorySlotTextView4);
         
         keyerEditText = (EditText)findViewById(R.id.keyerEditText);
         
@@ -112,30 +99,6 @@ public class AndroidomaticKeyerActivity extends Activity {
         	} else {
         		startMessage();
         	}
-        }
-    };
-    
-    private OnClickListener memorySlotButtonListener1 = new OnClickListener() {
-    	public void onClick(View v) {
-    		keyerEditText.setText(memorySlotTextView1.getText().toString());
-        }
-    };
-    
-    private OnClickListener memorySlotButtonListener2 = new OnClickListener() {
-    	public void onClick(View v) {
-    		keyerEditText.setText(memorySlotTextView2.getText().toString());
-        }
-    };
-    
-    private OnClickListener memorySlotButtonListener3 = new OnClickListener() {
-    	public void onClick(View v) {
-    		keyerEditText.setText(memorySlotTextView3.getText().toString());
-        }
-    };
-    
-    private OnClickListener memorySlotButtonListener4 = new OnClickListener() {
-    	public void onClick(View v) {
-    		keyerEditText.setText(memorySlotTextView4.getText().toString());
         }
     };
     
