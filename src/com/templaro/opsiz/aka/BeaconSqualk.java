1a35e8d9e085d108e1c8507661bed4e8fc4ed1ae
Jack Welch <dhakajack@gmail.com>
2012-01-25 00:52:48 +0700

    revised threading in BeaconSqualk

--- src/com/templaro/opsiz/aka/BeaconSqualk.java
+++ src/com/templaro/opsiz/aka/BeaconSqualk.java
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioTrack;
+import android.media.AudioTrack.OnPlaybackPositionUpdateListener;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -18,18 +21,57 @@ public class BeaconSqualk extends BroadcastReceiver {
 	private boolean qrss = false;
 	private String qrss_rate = "10";	
 	private String callsign ="URCALL";
+	private GeoHelper mGeo;
+	private String mMsg;
+	private Thread soundThread;
+	private AKASignaler signaler = AKASignaler.getInstance();
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		 LoadPrefs(context);
-		 Toast.makeText(context, "Activating Beacon: "+ beacon_text  +"Squalk! Squalk!", Toast.LENGTH_SHORT).show();
+		 Toast.makeText(context, "Activating Beacon: "+ beacon_text, Toast.LENGTH_SHORT).show();
 		 Log.i(TAG, "Squalk! "+ beacon_text);
+		 mMsg = beacon_text;
 		 
-		 MorsePlayer player = new MorsePlayer(hertz,speed);
-		 player.setMessage(beacon_text);
-		 player.playMorse();
-	}
+		 
+		 if(mMsg.matches(".*[#!]+.*")) {
+			 mGeo = new GeoHelper(context);
+			 mGeo.locationUpdatesOn();
+			 //Log.i(TAG,"waiting 1 minute for location update, then expanding geotags.");
+			 
+			 mMsg = mMsg.replaceAll("#",mGeo.currentDecimalLocation());
+			 mMsg = mMsg.replaceAll("!",mGeo.maidenheadGrid());
+			 mGeo.locationUpdatesOff(); //is this really necessary in that mGeo get nuked?
+		 }
+		 
+		 mMsg = mMsg.replaceAll("@",callsign);
 	
+		 soundThread = new Thread(new Runnable() {  // old soundThread gets garbage collected
+             @Override
+             public void run() {
+            	 MorsePlayer player = new MorsePlayer(hertz,speed);
+        		 player.setMessage(mMsg);
+           	     player.playMorse();
+           	     signaler.audioTrack.setNotificationMarkerPosition(signaler.msgSize / 2);
+           	     signaler.audioTrack.setPlaybackPositionUpdateListener(new OnPlaybackPositionUpdateListener() {
+           	    	 @Override
+           	    	 public void onPeriodicNotification(AudioTrack track) {
+           	    		 // nothing to do
+           	    	 }
+           	    	 @Override
+           	    	 public void onMarkerReached(AudioTrack track) {
+           	    		 Log.i(TAG, "AudioTrack played to end of message; time to die.");
+           	    		 signaler.killAudioTrack(); 
+           	    		 return;
+           	    	 }
+           	     });
+           	     Log.i(TAG, "Set marker-callback on " + (signaler.msgSize -2));
+             }
+		 });
+		 soundThread.start();
+		 
+	}
+		 
 	private void LoadPrefs(Context context) {
 		Log.i(TAG, "Loading saved preferences");
 		SharedPreferences prefs = 
@@ -49,5 +91,7 @@ public class BeaconSqualk extends BroadcastReceiver {
 	    	str.replaceAll("&amp;","&");
 	    	return str;
 	 }
+	 
+	 
 		
 }
